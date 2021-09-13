bee53efb7bd1fd2e26446593d65a38c5511b0d73
kengerero@yahoo.com <kengerero@yahoo.com>
2021-09-13 20:21:47 +0800

    done dynamic ui card list

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,6 @@
   <!DOCTYPE html>
   <html>
+
   <head>
     <title>BMHMS</title>
     <meta charset="utf-8">
@@ -11,21 +12,22 @@
     <link rel="icon" href="images\macawayan logo.png">
 
   </head>
+
   <body>
 
-  <div class="container-fluid">
+    <div class="container-fluid">
       <div class="row">
         <div class="col-md-12 d-flex flex-row align-items-center justify-content-between header">
-            <div class=" d-flex flex-row align-items-center title" >
-              <img src="images/macawayan logo.png" alt="irosin logo">
-              <p class="navbar-brand">BMHMS</p>
-            </div>
-            <div class="d-flex position-relative align-items-center account">
-              <p class="flex-shrink-1 user-name"><u>Juan Dela Cruz</u><br>NURSE</P>
-              <img src="images/profile.jpeg" alt="">
-            </div>
+          <div class=" d-flex flex-row align-items-center title">
+            <img src="images/macawayan logo.png" alt="irosin logo">
+            <p class="navbar-brand">BMHMS</p>
+          </div>
+          <div class="d-flex position-relative align-items-center account">
+            <p class="flex-shrink-1 user-name"><u>Juan Dela Cruz</u><br>NURSE</P>
+            <img src="images/profile.jpeg" alt="">
           </div>
         </div>
+      </div>
 
       <div class="row">
         <div class="col-md-3 sidebar">
@@ -37,20 +39,24 @@
             <a href="#"><i class="fas fa-file-alt"></i>Reports</a>
             <a href="#"><i class="fas fa-address-card"></i>Manage Account</a>
           </div>
+          <div class="position-relative d-grid flex-column log-out">
+            <a href="index.php" class="log-out"><i class="fas fa-sign-out-alt"></i>Log Out</a>
+          </div>
         </div>
 
-      <div class="col-md-9 content">
-        <div class="dashboard-menu">
+        <div class="col-md-9 content">
+          <div class="dashboard-menu">
             <a href="resident_profile.php"><i class="fas fa-id-badge"></i><br>Resident Profile</a>
             <a href="health_consultation.php"><i class="fas fa-heartbeat"></i><br>Health Consultation</a>
             <a href="#"><i class="fas fa-calendar-alt"></i><br>Events</a>
+          </div>
+          <hr>
         </div>
-        <hr>
       </div>
     </div>
-  </div>
 
-  <script src="bootstrap/js/jquery-3.4.1.min.js"></script>
-  <script src="bootstrap/js/bootstrap.js"></script>
+    <script src="bootstrap/js/jquery-3.4.1.min.js"></script>
+    <script src="bootstrap/js/bootstrap.js"></script>
   </body>
+
   </html>

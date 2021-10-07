a4bf6d7a0f9f11b39b8f791e1688df138e5c21b2
johnkenneth24 <kengerero@yahoo.com>
2021-10-07 08:07:22 +0800

    done print request for med

--- med_request.php
+++ med_request.php
@@ -118,10 +118,10 @@
                                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                         </div>
                                         <div class="modal-body">
-                                          <form class="form-group">
+                                          <form id="printThis" class="form-group">
                                             <h5 class="text-center">MEDICINE REQUEST FORM</h5>
                                             <br>
-                                            <div id="printThis" class="request_form">
+                                            <div  class="request_form">
                                             <p class="mb-1 fw-bold req_name">Requested By: <input type="text" class="border-top-0 border-end-0 border-start-0 border-dark text-uppercase" name="" value=""></p>
                                             <p class="mb-1 fw-bold req_name">Date Requested: <input type="date" class="border-top-0 border-end-0 border-start-0 border-dark text-uppercase req_date" name="" value=""></p>
                                             <p class="lh-sm fw-bold req_name">Request No.: <input type="text" class="border-top-0 border-end-0 border-start-0 border-dark text-uppercase" name="" value=""></p>
@@ -142,6 +142,10 @@
                                               </tbody>
                                             </table>
                                           </div>
+                                          <br>
+                                          <div class="d-flex flex-row-reverse signature-line ">
+                                          <p class="name_signature fw-bold mb-0 border-top ">Signature Over Printed Name</p>
+                                        </div>
                                           </form>
                                         </div>
                                         <div class="modal-footer">
